ad84433a0b5d3ccd2fc3b20159939ade9abb0f8a
Suei43 <chrisraphael576@gmail.com>
2023-07-13 08:22:30 +0100

    first day of c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - entry point
+ * printf - output message on the screen
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
