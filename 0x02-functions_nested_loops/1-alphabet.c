2849e1323895107c870dba9c6263cee4d9d96f20
Chris Raphael <chrisraphael576@gmail.com>
2023-07-18 14:39:12 +0100

    Create  1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - prints all the alphabet
+ * 
+*/
+void print_alphabet(void)
+{
+  char letter = 'a';
+
+  for (letter; letter <= 'z'; letter++)
+  _putchar(letter);
+  
+  _putchar('\n');
+}
