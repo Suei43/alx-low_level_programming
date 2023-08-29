23c016f66a2d9bfaded7367f285a5a19cb1aa47c
Suei43 <chrisraphael576@gmail.com>
2023-08-29 01:24:10 +0100

    0x13 -2

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * of a linked list.
+ * delete_nodeint_at_index - deletes the node at index of a linked list.
  * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
+ * @index: index of the list where the node is deleted.
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * Return: Returns 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * add_nodeint - add a new node at the beginning
- * of a linked list
+ * add_nodeint - add a new node at the beginning of a linked list
  * @head: head of a list.
  * @n: n element.
  *
- * Return: address of the new element. NUll if it failed.
+ * Return: returns the address of the new element or null if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
